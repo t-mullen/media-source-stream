a7c3f6a8029986cce718383a5da6286aee536721
RationalCoding <thomasmullendesign@gmail.com>
2017-03-27 04:26:30 -0400

    remove unnecessary file reader

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = MediaSourceStream
 
-/* globals MediaSource, Blob, FileReader */
+/* globals MediaSource */
 
 function MediaSourceStream (stream, opts) {
   var self = this
@@ -12,32 +12,15 @@ function MediaSourceStream (stream, opts) {
   opts.mimeType = opts.mimeType || 'video/webm; codecs="opus,vp8"'
 
   self.mediaSource = new MediaSource(opts)
-
   self._sourceBuffer = null
-  self._blob = null
 
   self.mediaSource.addEventListener('sourceopen', function (e) {
     self._sourceBuffer = self.mediaSource.addSourceBuffer(opts.mimeType)
   })
 
   stream.on('data', function (chunk) {
-    if (self._blob == null) {
-      self._blob = new Blob([chunk], {type: 'video/webm'})
-    } else {
-      self._blob = new Blob([self._blob, chunk], {type: 'video/webm'}) // Merge blobs
-    }
-
     if (!self._sourceBuffer || self._sourceBuffer.updating) return
-
-    var bl = self._blob
-    self._blob = null
-
-    var fileReader = new FileReader()
-    fileReader.onload = function (e) {
-      if (self._sourceBuffer.updating) return
-      self._sourceBuffer.appendBuffer(new Uint8Array(fileReader.result))
-    }
-    fileReader.readAsArrayBuffer(bl)
+    self._sourceBuffer.appendBuffer(chunk)
   })
 }
 
@@ -48,5 +31,4 @@ MediaSourceStream.prototype.destroy = function () {
   self.mediaSource = null
 
   self._sourceBuffer = null
-  self._blob = null
 }
